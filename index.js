db73d9f2e227d45b1cc0d54f83bdc2779f98d28d
Garima Bhayana <garimaabhayanaa@gmail.com>
2025-07-24 16:23:49 +0530

    Created index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const runInference = require('./inference.js'); 
+
+// CORS setup for Vercel frontend
+const allowedOrigins = ['https://ai-textsummarizer.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/inference', async (req, res) => {
+  console.log('/inference route hit');
+  const { text, task, tone } = req.body;
+  if (!text || !task || !tone) {
+    return res.status(400).send("Missing 'text', 'task', or 'tone'");
+  }
+  try {
+    console.log('starting running inference.js')
+    const output = await runInference(text, task, tone);
+    res.send(output);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
